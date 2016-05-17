fec540732605b557435bce94f42918f1fffb4df8
Jochen Grün <jochen.gruen@googlemail.com>
2016-05-17 22:49:10 +0200

    First angular component working.

---
+++ src/Aunoa.Web/AppTS/app.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'aunoa-app',
+    template: '<h1>Aunoa</h1>'
+})
+export class AppComponent { }
---
+++ src/Aunoa.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Routing;
+
+namespace Aunoa.Web.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            return Redirect("~/Home/About");
+        }
+    }
+}
